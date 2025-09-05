850de07d60ede2a18b3f3f901b882027a82d4538
sd-1211 <sridevivg12@gmail.com>
2025-09-05 10:04:33 +0530

    Added Chat_Bot.py

---
+++ Chat_Bot.py
@@ -0,0 +1,21 @@
+def chat_bot(user_input):
+    user_input=user_input.lower()
+    if "hello" in user_input or "hi" in user_input or "hey" in user_input:
+        return "Hello..How can I help You Today?"
+    elif "how are you" in user_input:
+        return "Iam fine..thanks!!"
+    elif "bye" in user_input:
+        return "Goodbye.Have a nice day"
+    elif "time" in user_input:
+        from datetime import datetime
+        return "current time is "+ datetime.now().strftime("%H:%M:%S")
+    else:
+        return "Iam not sure how to respond to that yet"
+print("Chatbot is running! type 'bye' to quit.\n")
+while True:
+    user=input("You:")
+    if "bye" in user.lower():
+        print("Chatbot:Goodbye.Have a nice day")
+        break
+    else:
+        print("Chatbot:",chat_bot(user))
